a9fc771c8f94f5336e82685b218737b02b7cd07b
Giuseppe Moscarella <giuseppe.moscarella@gmail.com>
2016-11-20 04:26:20 +0100

    Added failing test for route with back tracking

--- src/main/java/guru/bytecode/rrp/Quester.java
+++ src/main/java/guru/bytecode/rrp/Quester.java
@@ -12,24 +12,30 @@ class Quester {
     List<Move> findRoute(Room startingRoom, Set<Item> itemsToCollect) {
         Objects.requireNonNull(startingRoom);
         Objects.requireNonNull(itemsToCollect);
-
-        return findRouteWithDepthFirstSearch(startingRoom, new HashSet<>(itemsToCollect));
+        
+        HashSet<Item> remainingItems = new HashSet<>(itemsToCollect);
+        List<Move> route = findRouteWithDepthFirstSearch(startingRoom, remainingItems);
+        return remainingItems.isEmpty()? route : emptyList();
     }
 
     private List<Move> findRouteWithDepthFirstSearch(Room startingRoom, Set<Item> itemsToCollect) {
         Set<Item> startingRoomItems = new HashSet<>(startingRoom.getItems());
         if (itemsToCollect.isEmpty() || startingRoomItems.containsAll(itemsToCollect)) {
+            System.out.println("if");
             itemsToCollect.removeAll(startingRoomItems);
             return singletonList(new Move(startingRoom, startingRoomItems));
         } else {
+            System.out.println("else");
             startingRoomItems.retainAll(itemsToCollect);
             itemsToCollect.removeAll(startingRoomItems);
             List<Move> route = new ArrayList<>();
             route.add(new Move(startingRoom, startingRoomItems));
+            System.out.printf("route: %s%n", route);
             startingRoom.getNeighbors().forEach((neighbor) -> {
                 route.addAll(findRouteWithDepthFirstSearch(neighbor, itemsToCollect));
+                System.out.printf("route: %s%n", route);
             });
-            return itemsToCollect.isEmpty() ? route : emptyList();
+            return route;
         }
     }
 
--- src/test/java/guru/bytecode/rrp/QuesterTest.java
+++ src/test/java/guru/bytecode/rrp/QuesterTest.java
@@ -36,32 +36,32 @@ public class QuesterTest {
     public void emptyRoute() {
         Room startingRoom = mockRoom();
         Set<Item> itemsToCollect = singleton(mock(Item.class));
-        
+
         List<Move> route = player1.findRoute(startingRoom, itemsToCollect);
-        
+
         assertThat(route).isEmpty();
     }
 
     @Test
     public void nothingToCollect() {
         Room startingRoom = mockRoom();
-        
+
         List<Move> route = player1.findRoute(startingRoom, emptySet());
-        
+
         assertThat(route).containsOnly(new Move(startingRoom, emptySet()));
     }
-    
+
     @Test
     public void allItemsAreInStartingRoom() {
         Item item = mock(Item.class);
         Room startingRoom = mockRoom(item);
         Set<Item> itemsToCollect = singleton(item);
-        
+
         List<Move> route = player1.findRoute(startingRoom, itemsToCollect);
-        
+
         assertThat(route).containsOnly(new Move(startingRoom, itemsToCollect));
     }
-    
+
     @Test
     public void route2() {
         Item item = mock(Item.class);
@@ -69,15 +69,15 @@ public class QuesterTest {
         Room neighborRoom = mockRoom(item);
         when(startingRoom.getNeighbors()).thenReturn(singleton(neighborRoom));
         Set<Item> itemsToCollect = singleton(item);
-        
+
         List<Move> route = player1.findRoute(startingRoom, itemsToCollect);
-        
+
         assertThat(route).containsExactly(
                 new Move(startingRoom, emptySet()),
                 new Move(neighborRoom, itemsToCollect)
         );
     }
-    
+
     @Test
     public void route3() {
         Item item1 = mock(Item.class);
@@ -88,26 +88,49 @@ public class QuesterTest {
         connect(startingRoom, neighbor1);
         connect(neighbor1, neighbor2);
         Set<Item> itemsToCollect = setOf(item1, item2);
-        
+
         List<Move> route = player1.findRoute(startingRoom, itemsToCollect);
-        
+
         assertThat(route).containsExactly(
                 new Move(startingRoom, emptySet()),
                 new Move(neighbor1, singleton(item1)),
                 new Move(neighbor2, singleton(item2))
         );
     }
-    
+
+    @Test
+    public void routeWithBackTracking() {
+        Item item1 = mock(Item.class);
+        Item item2 = mock(Item.class);
+        Room startingRoom = mockRoom();
+        Room neighbor1 = mockRoom(item1);
+        Room neighbor2 = mockRoom(item2);
+        connect(startingRoom, neighbor1, neighbor2);
+        Set<Item> itemsToCollect = setOf(item1, item2);
+
+        List<Move> route = player1.findRoute(startingRoom, itemsToCollect);
+
+        assertThat(route)
+//                .hasSize(4)
+                .containsSequence(
+                        new Move(startingRoom, emptySet()),
+                        new Move(neighbor1, singleton(item1))
+                ).containsSequence(
+                        new Move(startingRoom, emptySet()),
+                        new Move(neighbor2, singleton(item2))
+                );
+    }
+
     private void connect(Room room, Room... neighbors) {
         when(room.getNeighbors()).thenReturn(setOf(neighbors));
     }
-    
+
     private Room mockRoom(Item... items) {
         Room room = mock(Room.class);
         when(room.getItems()).thenReturn(setOf(items));
         return room;
     }
-    
+
     private static <T> Set<T> setOf(T... elements) {
         return Stream.of(elements).collect(toSet());
     }
