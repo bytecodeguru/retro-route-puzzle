e70d6cfc5825c69a1ca006d3a5cd24a8970f755a
Giuseppe Moscarella <giuseppe.moscarella@gmail.com>
2016-11-20 05:20:43 +0100

    Refactored Depth First Search

--- src/main/java/guru/bytecode/rrp/Quester.java
+++ src/main/java/guru/bytecode/rrp/Quester.java
@@ -1,7 +1,9 @@
 package guru.bytecode.rrp;
 
+import java.util.ArrayDeque;
 import java.util.ArrayList;
 import static java.util.Collections.*;
+import java.util.Deque;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Objects;
@@ -9,33 +11,32 @@ import java.util.Set;
 
 class Quester {
 
-    List<Move> findRoute(Room startingRoom, Set<Item> itemsToCollect) {
-        Objects.requireNonNull(startingRoom);
+    List<Move> findRoute(Room startRoom, Set<Item> itemsToCollect) {
+        Objects.requireNonNull(startRoom);
         Objects.requireNonNull(itemsToCollect);
         
         HashSet<Item> remainingItems = new HashSet<>(itemsToCollect);
-        List<Move> route = findRouteWithDepthFirstSearch(startingRoom, remainingItems);
-        return remainingItems.isEmpty()? route : emptyList();
+        List<Move> route = new ArrayList<>();
+        Deque<Room> rooms = new ArrayDeque<>();
+        rooms.offerFirst(startRoom);
+        findRouteWithGreedyDepthFirstSearch(rooms, remainingItems, route);
+        return remainingItems.isEmpty() ? route : emptyList();
     }
 
-    private List<Move> findRouteWithDepthFirstSearch(Room startingRoom, Set<Item> itemsToCollect) {
-        Set<Item> startingRoomItems = new HashSet<>(startingRoom.getItems());
-        if (itemsToCollect.isEmpty() || startingRoomItems.containsAll(itemsToCollect)) {
-            System.out.println("if");
-            itemsToCollect.removeAll(startingRoomItems);
-            return singletonList(new Move(startingRoom, startingRoomItems));
-        } else {
-            System.out.println("else");
-            startingRoomItems.retainAll(itemsToCollect);
-            itemsToCollect.removeAll(startingRoomItems);
-            List<Move> route = new ArrayList<>();
-            route.add(new Move(startingRoom, startingRoomItems));
-            System.out.printf("route: %s%n", route);
-            startingRoom.getNeighbors().forEach((neighbor) -> {
-                route.addAll(findRouteWithDepthFirstSearch(neighbor, itemsToCollect));
-                System.out.printf("route: %s%n", route);
+    private void findRouteWithGreedyDepthFirstSearch(Deque<Room> rooms, Set<Item> itemsToCollect, List<Move> route) {
+        Room room = rooms.pollFirst();
+        Set<Item> collectedItems = new HashSet<>(room.getItems());
+        collectedItems.retainAll(itemsToCollect);
+        route.add(new Move(room, collectedItems));
+        itemsToCollect.removeAll(collectedItems);
+        
+        if (!itemsToCollect.isEmpty()) {
+            room.getNeighbors().forEach((neighbor) -> {
+                rooms.offerFirst(neighbor);
             });
-            return route;
+            if (!rooms.isEmpty()) {
+                findRouteWithGreedyDepthFirstSearch(rooms, itemsToCollect, route);
+            }
         }
     }
 
